dbc1820f00edb8854839c1a29b1672bdd62780e7
Sophia Liu <sophialiu7436@gmail.com>
2025-09-23 02:01:08 -0500

    change to hashrouter

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export function NotFound() {
         <TbError404 size={120} className="inline mb-1 text-slate-500 " />
       </div>
       <h1 className="text-5xl font-bold mb-6">Oops!</h1>
-      <p>Sorry the page you're looking for doesn't exist :(</p>
+      <p>Sorry, the page you're looking for doesn't exist :(</p>
       <p>
         Don't worry. You can go{" "}
         <a href="/" className="link">
