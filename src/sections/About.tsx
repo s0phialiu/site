cd091257f900c46aa209c80ba9079c08dca24d3f
Sophia Liu <sophialiu7436@gmail.com>
2025-09-23 15:09:04 -0500

    add photo link

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,7 +9,14 @@ function About() {
             I am a senior at the University of Illinois at Urbana-Champaign, pursuing a Bachelor of Science in Computer Science.
         </p>
         <p>
-        I have a passion for systems programming and cybersecurity, and I enjoy working on projects that challenge me to learn and grow in these fields. In my free time, I enjoy painting and taking landscape/wildlife photography. 
+        I have a passion for systems programming and cybersecurity, and I enjoy working on projects that challenge me to learn and grow in these fields. In my free time, I enjoy painting and taking landscape/wildlife photography. Check out some of my photos from hikes{" "}   
+        <a
+          href="https://vsco.co/s0phialiu/gallery"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <strong>here!</strong>
+        </a>
         </p>
         <p>I'm currently looking for full-time Software Engineer or Security Engineer roles. Feel free to reach out through email or LinkedIn!</p>
     </div>
