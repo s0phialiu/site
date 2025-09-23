249823cc96a9b7c349b9bbff0f81270b0b994301
Sophia Liu <sophialiu7436@gmail.com>
2025-09-23 02:09:17 -0500

    update to work with netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, NotFound } from './pages';
 import './App.css'
 
